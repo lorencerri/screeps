d9a54537b75c7e1285cb023e4f2ac3b631d877be
Loren Cerri <lorencerri@gmail.com>
2021-10-14 15:05:26 -0400

    Rewrite tower structure

--- src/structures/tower.js
+++ src/structures/tower.js
@@ -1,30 +1,26 @@
-// TODO: Rewrite tower file
-
 const Tower = {
-	run: function (tower) {
-		// Find closest hostile creep
+	new: function (tower) {
+		// Find the closest hostile creep
 		const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
-		// If found, attack
-		if (closestHostile) {
-			return tower.attack(closestHostile);
-		}
+		// If there is a hostile creep, attack it
+		if (closestHostile) tower.attack(closestHostile);
 
-		// Find closest damaged structure
+		// Find the closest damaged structure
 		const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
 			filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL
 		});
 
+		// If there is a damaged structure, repair it
+		if (closestDamagedStructure) return tower.repair(closestDamagedStructure);
+
+		// Find the closest damaged wall
 		const closestDamagedWall = tower.pos.findClosestByRange(FIND_STRUCTURES, {
 			filter: (s) => s.hits < s.hitsMax && (s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART) && s.hits <= 20000
 		});
 
-		// If found, repair
-		if (closestDamagedStructure) {
-			tower.repair(closestDamagedStructure);
-		} else if (closestDamagedWall) {
-			tower.repair(closestDamagedWall);
-		}
+		// If there is a damaged wall, repair it
+		if (closestDamagedWall) return tower.repair(closestDamagedWall);
 	}
 };
 
